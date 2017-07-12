cf613fead14eada16cbfb29e6fdfdcabd54a9f88
Minho Kim <minho1a@hanmail.net>
2017-07-12 15:53:24 +0900

    Implement pubmed crawler.

---
+++ crawler/pubmed_crawler.py
@@ -0,0 +1,40 @@
+
+# coding: utf-8
+
+# In[107]:
+
+
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+
+# In[108]:
+
+
+main_driver = webdriver.Chrome('/home/kmh/tools/chromedriver')
+sub_driver = webdriver.Chrome('/home/kmh/tools/chromedriver')
+#driver.implicitly_wait(3)
+
+
+# In[109]:
+
+
+main_driver.get('https://www.ncbi.nlm.nih.gov/pubmed/?term=hasstructuredabstract')
+
+
+# In[110]:
+
+
+for i in range(0, 181260, 1) :
+    pmid_list = BeautifulSoup(main_driver.page_source, 'html.parser').select('.rprtid dd')
+
+    for pmid in pmid_list :
+        sub_driver.get('https://www.ncbi.nlm.nih.gov/pubmed/' + pmid.text.strip() + '?report=xml&format=text')
+        xml = BeautifulSoup(BeautifulSoup(sub_driver.page_source, 'html.parser').find('pre').text, 'html.parser')
+
+        for node in xml.findAll('abstracttext') :
+            print(node['label'])
+            
+        print('\n')
+    main_driver.find_element_by_class_name('next').click()
+
