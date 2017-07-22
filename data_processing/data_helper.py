f7bb5bcf7c49fffdd954c94c069296dfc4e8ea86
yoon-jae <yoonjae.cho92@gmail.com>
2017-07-22 22:24:53 +0900

    Edit data_helper.py indentation

--- data_processing/data_helper.py
+++ data_processing/data_helper.py
@@ -28,7 +28,7 @@ class Main:
 
             try:
                 self.parser.set_article(path_target + '/' + xml)
-                sql = self.db_manager.sql_insert_into_pmid(self.parser.get_pmid(), self.parser.get_abstract())                         + self.db_manager.sql_insert_into_abstract(self.parser.get_pmid(), self.parser.get_su(), self.parser.get_ppub())                         + self.db_manager.sql_get_all_sentence(self.parser.get_pmid(), self.parser.get_map_label(), self.parser.get_origin_label(), self.parser.get_sentence())
+                sql = self.db_manager.sql_insert_into_pmid(self.parser.get_pmid(), self.parser.get_abstract()) + self.db_manager.sql_insert_into_abstract(self.parser.get_pmid(), self.parser.get_su(), self.parser.get_ppub()) + self.db_manager.sql_get_all_sentence(self.parser.get_pmid(), self.parser.get_map_label(), self.parser.get_origin_label(), self.parser.get_sentence())
                 self.db_manager.commit(sql.encode())
 
             except KeyboardInterrupt:
